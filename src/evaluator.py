2dd68811a80bedf6ab6c2b90b616942d6cb3a965
Abhinav Rajesh <abhinav.rajesh@kalvium.community>
2026-07-27 22:11:09 +0530

    Switched from Groq to Gemini API

--- src/evaluator.py
+++ src/evaluator.py
@@ -1,34 +1,28 @@
 import os
 from dotenv import load_dotenv
-from groq import Groq
+from google import genai
 
 from prompt import SYSTEM_PROMPT
-from schema import Evaluation
 
 load_dotenv()
 
-client = Groq(
-    api_key=os.getenv("GROQ_API_KEY")
-)
+client = genai.Client(api_key=os.getenv("GEMINI_API_KEY"))
 
+import json
 
 def evaluate_transcript(transcript: str):
-    response = client.chat.completions.create(
-        model="llama-3.3-70b-versatile",
-        messages=[
-            {
-                "role": "system",
-                "content": SYSTEM_PROMPT,
-            },
-            {
-                "role": "user",
-                "content": transcript,
-            },
-        ],
-        temperature=0,
-        response_format={"type": "json_object"},
+
+    prompt = f"""
+{SYSTEM_PROMPT}
+
+Transcript:
+
+{transcript}
+"""
+
+    response = client.models.generate_content(
+        model="gemini-2.5-flash",
+        contents=prompt,
     )
 
-    content = response.choices[0].message.content
-
-    return content, response
+    return response.text, response
